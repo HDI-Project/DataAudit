9f6b88b8038c9f280f131ef2416fb747aa5af013
BFar <Bryan@skylabapps.com>
2018-08-31 16:19:55 -0400

    Increment PyPi Version

--- setup.py
+++ setup.py
@@ -52,6 +52,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/HDI-Project/DataAudit',
-    version='0.1.0-dev',
+    version='0.1.1-dev',
     zip_safe=False,
 )
