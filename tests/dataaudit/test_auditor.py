a2daffb883b35ae787fd791d8da28215c42c11a0
Sarah Alnegheimish <sarahalnegheimish@gmail.com>
2019-01-04 14:58:26 -0500

    added frequency check function and mean

--- tests/dataaudit/test_auditor.py
+++ tests/dataaudit/test_auditor.py
@@ -15,6 +15,9 @@ def create_df():
                          'birthdate': ['10/21/2000', 'unknown', '1/10/2000', '9/16/2000'],
                          'address': ['undefined', '4060246413000', '406026413022', '40677564000']})
 
+@pytest.fixture()
+def total_nans_df():
+    return pd.DataFrame({'identifier': [0, 1, 2, 3]})
 
 @pytest.fixture()
 def get_check_list():
@@ -23,13 +26,13 @@ def get_check_list():
             {'birthdate': [{'nan': {'number': 0, 'percenatge': '0.00%'}}]}]
 
 
-def test_check_total_nans(create_df):
+def test_check_total_nans(total_nans_df):
     expected_output = [{'Patient': [{'identifier': {'nan': {'number': 0, 'percenatge': '0.00%'}}},
                                     {'gender': {'nan': {'number': 2, 'percenatge': '50.00%'}}},
                                     {'birthdate': {'nan': {'number': 1, 'percenatge': '25.00%'}}},
                                     {'address': {'nan': {'number': 1, 'percenatge': '25.00%'}}}]}]
     expected_output = expected_output[0]['Patient'][0]
-    output = auditor.Auditor().check_total_nans('Patient', create_df)
+    output = auditor.Auditor().check_total_nans('Patient', total_nans_df)
     output = output[0]['Patient'][0]
 
     assert output['identifier'] == expected_output['identifier']
@@ -55,6 +58,10 @@ def test_find_percentage():
     assert auditor.Auditor().find_percentage(
         'gender', ['null', 'Nan', 'male', 'female']) == expected_output
 
+def test_find_frequency():
+    expected_output = {'female': 1, 'male': 2}
+    assert auditor.Auditor().find_frequency(
+        ['female', 'male', 'male']) == expected_output
 
 def test_find_minimum_error():
     with pytest.raises(TypeError):
@@ -72,6 +79,15 @@ def test_find_maximum_error():
 
 def test_find_maximum():
     assert auditor.Auditor().find_maximum([2, nan, 3]) == {'max': 3}
+
+
+def test_find_mean_error():
+    with pytest.raises(TypeError):
+        auditor.Auditor().find_mean([2, "f", 3])
+
+
+def test_find_mean():
+    assert auditor.Auditor().find_mean([2, 4, 3]) == {'mean': 3}
 
 
 def test_compare_distributions():
